6ad16cb56eed46ea85d1dc7de5b747c2645a086f
tower-town <FibonacciNuture@foxmai.com>
2021-12-20 21:47:12 +0800

    add filter of iterator and some compromise

--- src/paper_down.py
+++ src/paper_down.py
@@ -1,29 +1,16 @@
 
 import asyncio
-import os
 from download import paper_downing
 from parse import argument
              
 
-async def main(x1, x2, x3, x4,arg):
+async def main():
 
-    await asyncio.gather(
-        paper_downing(x1,arg),
-        paper_downing(x2,arg),
-        paper_downing(x3,arg),
-        paper_downing(x4,arg)
-    )
+    filename,export =argument()
+    print('begin')
+    for task in filename:
+        await asyncio.gather(paper_downing(task,export))
+    print('end')
 
-filename,export =argument()
-step = 4
-box = [filename[i:i+step] for i in range(0, len(filename), step)]
+asyncio.run(main())
 
-
-def gf(a=0, b=0, c=0, d=0):
-    return [a, b, c, d]
-
-print('begin')
-for bl in box:
-    bl = gf(*bl)
-    asyncio.run(main(*bl,arg=export))
-print('finish')
--- src/parse.py
+++ src/parse.py
@@ -2,6 +2,7 @@ import argparse
 import os
 
 def argument():
+    
     filename = list()
     parser = argparse.ArgumentParser()
     group = parser.add_mutually_exclusive_group(required=True)
@@ -10,13 +11,12 @@ def argument():
     parser.add_argument('--export','-e',help='export path of download paper',default=os.path.dirname(str(os.getcwd())))
     args = parser.parse_args()
     exportpath = args.export
+
     if args.path != None:
         with open(str(args.path),mode='r',encoding='utf-8') as file:
-            for i in file.readlines():
-                 if not str(i).startswith('#') and not str(i) == '\n':
-                    filename.append(str(i).strip())
-
+            filename = filter(lambda index :(not (str(index).startswith('#') or str(index) == '\n')),file.readlines())
+            
     if args.name != None:
         filename.append(str(args.name).strip())
 
-    return filename,exportpath
+    return list(filename),exportpath
